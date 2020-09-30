2d9fe4bdc02b9a16592904f31eb9e150adf7de75
mtfr1 <mateusfigueiredo@dcc.ufmg.br>
2020-09-30 16:06:53 -0300

    commit 4

--- Dollar.java
+++ Dollar.java
@@ -1,8 +1,13 @@
 class Dollar extends Money {
-    Dollar(int amount) {
-        this.amount = amount;
-    }
-    Money times(int multiplier) {
-        return new Dollar(amount * multiplier);
-    }
+	Dollar(int amount, String currency) {
+		super(amount, currency);
+	}
+
+	String currency() {
+		return currency;
+	}
+
+	Money times(int multiplier) {
+		return Money.dollar(amount * multiplier);
+	}
 }		
