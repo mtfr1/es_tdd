afa412bebe5d1f04e2d8968d6e6d95d74b946f40
mtfr1 <mateusfigueiredo@dcc.ufmg.br>
2020-09-30 16:15:21 -0300

    commit 5

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 class Main {
-    public static void main(String[] args) {
-	    Tests tests = new Tests();
-	    tests.testMultiplication();
-	    tests.testFrancMultiplication();
-	    tests.testEquality();
-	    tests.testCurrency();
-    }
-  }
+	public static void main(String[] args) {
+		Tests tests = new Tests();
+		tests.testMultiplication();
+		tests.testEquality();
+		tests.testCurrency();
+	}
+}
